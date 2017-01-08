085fc0a608c71f1ebd6fa0571feeea3c1cc0ca4b
cmroanirgo <cmroanirgo@users.noreply.github.com>
2017-01-08 15:48:15 +1100

    initial website build

---
+++ README.md
@@ -0,0 +1,5 @@
+# ergo-cms
+
+Please visit [https://ergo-cms.github.io](https://ergo-cms.github.io).
+
+
---
+++ config.ergo.js
@@ -0,0 +1,25 @@
+/*
+This is a minimal skeleton config for ergo cms. See online for more options
+*/
+
+module.exports = {
+	// out_path: "docs"
+	default_fields: {
+		  site_url: "https://ergo-cms.github.io/" 
+		, title: "ergo-cms ∴" // this goes in <title>tags generally, but also used in rss 
+	  }
+	, plugin_optins: {
+		textile: {breaks:true}
+	}
+	, default_post_type: "page"
+	, post_types: {
+		article: {
+			  layout: "article.html"
+			  , path: "articles"
+		  }
+		, page: {
+			  layout: "article.html"
+		  }
+	}
+	
+};
